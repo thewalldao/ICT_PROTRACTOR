b80fd399b3b38c97cb66bb772b75a4958c850d8c
STI2K\satt20.04.03 <thewalldao@gmail.com>
2020-12-28 12:28:40 +0700

    add some xpath

---
+++ built/conf/register_TC01_config.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    specs: [
+        'E:/TUANDAO/PROJECT/SimpleProtractorTest/built/TestCases/register-TC01.js'
+    ],
+    framework: 'jasmine',
+    capabilities: {
+        browserName: 'chrome',
+    },
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    directConnect: false,
+    SELENIUM_PROMISE_MANAGER: false,
+};
+//# sourceMappingURL=register_TC01_config.js.map
