794590a499c950a9c29b23b34e291f5f39ab190f
STI2K\satt20.04.03 <thewalldao@gmail.com>
2020-12-28 15:02:51 +0700

    update register-testcases

---
+++ built/conf/register-testcases/register_TC001_02_config.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    specs: [
+        'E:/TUANDAO/PROJECT/SimpleProtractorTest/built/TestCases/register-testcases/register-TC001-02.js'
+    ],
+    framework: 'jasmine',
+    capabilities: {
+        browserName: 'chrome',
+    },
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    directConnect: false,
+    SELENIUM_PROMISE_MANAGER: false,
+};
+//# sourceMappingURL=register_TC001_02_config.js.map
