d4331c517d5eefd4dfcd6f3a5d5fe42eb411ed92
ThewallDao <thewalldao@gmail.com>
2021-02-03 16:31:04 +0700

    Update test_scheme.scm and ~$view-config-v1.docx

--- Document/test_scheme.scm
+++ Document/test_scheme.scm
@@ -1,6 +1,8 @@
 #lang sicp
 
 ;; EX 1.1
+(display "-----------------------\n")
+(display "EX 1.1:\n")
 
 10
 (+ 5 3 4)
@@ -28,9 +30,64 @@
          (else -1))
    (+ a 1))
 ;; EX 1.2
+(display "-----------------------\n")
+(display "EX 1.2:\n")
 
 (/ (+ 5 4
-         (- 2
-             (- 3
-                 (+ 6 (/ 4 5)))))
-   (* 3 (- 6 2) (- 2 7)))
+      (- 2
+         (- 3
+            (+ 6 (/ 4 5)))))
+   (* 3
+      (- 6 2)
+      (- 2 7)))
+
+;; EX 1.3
+(display "-----------------------\n")
+(display "EX 1.3:\n")
+
+(define (square x) (* x x))
+
+(define (squareSum x y) (+ (square x) (square y)))
+
+(define (sumOfLargestTwoSquared x y z)
+  (cond ((and (>= (+ x y) (+ y z)) (>= (+ x y) (+ x z))) (squareSum x y))
+        ((and (>= (+ x z) (+ y z)) (>= (+ x z) (+ x y))) (squareSum x z))
+        (else (squareSum y z))
+        )
+  )
+(sumOfLargestTwoSquared 5 6 7)
+
+(display "-----------------------\n")
+(display "EX 1.4:\n")
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+;; (a-plus-abs-b 1 -3)
+;; ((if (> -3 0) + -) 1 -3)
+;; ((if #f + -) 1 -3)
+;; (- 1 + 3)
+
+;; (a-plus-abs-b 1 3)
+;; ((if (> 3 0) + -) 1 3)
+;; ((if #t + -) 1 3)
+;; (+ 1 3)
+
+(a-plus-abs-b 4 -5)
+
+(display "-----------------------\n")
+(display "EX 1.5:\n")
+
+(define (p) (p))
+(define (test x y)
+  (if (= x 0) 0 y))
+
+;; with applicative-order the evaluation of (test 0 (p))
+;; never terminates, because (p) is infinitely expanded to itself
+;; (test 0 (p))
+
+;; Using normal-order evaluation, the expression evaluates, step by step, to 0
+;; (test 0 (p))
+;; (if (= 0 0) 0 (p))
+;; (if #t 0 (p))
+;; 0
